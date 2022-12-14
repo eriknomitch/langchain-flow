6dbbf557e296e5be5a14ebd8c2459c3fd984fddf
Erik Nomitch <enomitch@gmail.com>
2022-12-14 04:11:01 -0600

    Initial commit with basic flow and add

---
+++ src/Flow.tsx
@@ -0,0 +1,73 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  Node,
+  addEdge,
+  Background,
+  Edge,
+  Connection,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+
+import CustomNode from "./CustomNode";
+
+import "reactflow/dist/style.css";
+
+const initialNodes: Node[] = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Node 1" },
+    position: { x: 250, y: 5 },
+  },
+  { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 100 } },
+  { id: "3", data: { label: "Node 3" }, position: { x: 400, y: 100 } },
+];
+
+const initialEdges: Edge[] = [
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e1-3", source: "1", target: "3" },
+];
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const BasicFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((els) => addEdge(params, els)),
+    [setEdges]
+  );
+
+  const onAdd = () => {
+    //setNodes((els) => AudioDes)
+    const newNode = {
+      id: "4",
+      data: { label: "Node 4" },
+      position: { x: 400, y: 200 },
+    };
+
+    const newNodes = [...nodes, newNode];
+    setNodes(newNodes);
+  };
+
+  return (
+    <>
+      <button onClick={onAdd}>Add</button>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+      >
+        <Background />
+      </ReactFlow>
+    </>
+  );
+};
+
+export default BasicFlow;
